e783965fce0452a00f6a55b4feee79d47688f6f5
Stephane Lafontaine <stephane@medqigong.ca>
2022-02-05 15:52:38 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Stephane Lafontaine on 2022-02-05.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
